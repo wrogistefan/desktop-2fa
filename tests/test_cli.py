39548753dc381514c56354df71d85e65225aeea5
wrogistefan <lukasz.perek@outlook.com>
2025-12-27 22:24:25 +0100

    Fix tests/test_cli.py:8: note: Use "-> None"

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,7 @@ from desktop_2fa.cli.main import app
 runner = CliRunner()
 
 
-def test_list():
+def test_list() -> None:
+
     result = runner.invoke(app, ["list"])
     assert result.exit_code == 0
